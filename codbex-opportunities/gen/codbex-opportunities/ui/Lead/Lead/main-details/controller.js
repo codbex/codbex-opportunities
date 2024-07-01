978476022ecc18137d2a293e6be2114820c48174
TIVMOF <tomyivanov06@gmail.com>
2024-07-01 04:01:47 +0000

    chore: Regenerated from edm

---
+++ codbex-opportunities/gen/codbex-opportunities/ui/Lead/Lead/main-details/controller.js
@@ -0,0 +1,109 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-opportunities.Lead.Lead';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/ts/codbex-opportunities/gen/codbex-opportunities/api/Lead/LeadService.ts";
+	}])
+	.controller('PageController', ['$scope', 'Extensions', 'messageHub', 'entityApi', function ($scope, Extensions, messageHub, entityApi) {
+
+		$scope.entity = {};
+		$scope.forms = {
+			details: {},
+		};
+		$scope.formHeaders = {
+			select: "Lead Details",
+			create: "Create Lead",
+			update: "Update Lead"
+		};
+		$scope.action = 'select';
+
+		//-----------------Custom Actions-------------------//
+		Extensions.get('dialogWindow', 'codbex-opportunities-custom-action').then(function (response) {
+			$scope.entityActions = response.filter(e => e.perspective === "Lead" && e.view === "Lead" && e.type === "entity");
+		});
+
+		$scope.triggerEntityAction = function (action) {
+			messageHub.showDialogWindow(
+				action.id,
+				{
+					id: $scope.entity.Id
+				},
+				null,
+				true,
+				action
+			);
+		};
+		//-----------------Custom Actions-------------------//
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("clearDetails", function (msg) {
+			$scope.$apply(function () {
+				$scope.entity = {};
+				$scope.optionsIndustry = [];
+				$scope.optionsStatus = [];
+				$scope.optionsOwner = [];
+				$scope.action = 'select';
+			});
+		});
+
+		messageHub.onDidReceiveMessage("entitySelected", function (msg) {
+			$scope.$apply(function () {
+				$scope.entity = msg.data.entity;
+				$scope.optionsIndustry = msg.data.optionsIndustry;
+				$scope.optionsStatus = msg.data.optionsStatus;
+				$scope.optionsOwner = msg.data.optionsOwner;
+				$scope.action = 'select';
+			});
+		});
+
+		messageHub.onDidReceiveMessage("createEntity", function (msg) {
+			$scope.$apply(function () {
+				$scope.entity = {};
+				$scope.optionsIndustry = msg.data.optionsIndustry;
+				$scope.optionsStatus = msg.data.optionsStatus;
+				$scope.optionsOwner = msg.data.optionsOwner;
+				$scope.action = 'create';
+			});
+		});
+
+		messageHub.onDidReceiveMessage("updateEntity", function (msg) {
+			$scope.$apply(function () {
+				$scope.entity = msg.data.entity;
+				$scope.optionsIndustry = msg.data.optionsIndustry;
+				$scope.optionsStatus = msg.data.optionsStatus;
+				$scope.optionsOwner = msg.data.optionsOwner;
+				$scope.action = 'update';
+			});
+		});
+		//-----------------Events-------------------//
+
+		$scope.create = function () {
+			entityApi.create($scope.entity).then(function (response) {
+				if (response.status != 201) {
+					messageHub.showAlertError("Lead", `Unable to create Lead: '${response.message}'`);
+					return;
+				}
+				messageHub.postMessage("entityCreated", response.data);
+				messageHub.postMessage("clearDetails", response.data);
+				messageHub.showAlertSuccess("Lead", "Lead successfully created");
+			});
+		};
+
+		$scope.update = function () {
+			entityApi.update($scope.entity.Id, $scope.entity).then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("Lead", `Unable to update Lead: '${response.message}'`);
+					return;
+				}
+				messageHub.postMessage("entityUpdated", response.data);
+				messageHub.postMessage("clearDetails", response.data);
+				messageHub.showAlertSuccess("Lead", "Lead successfully updated");
+			});
+		};
+
+		$scope.cancel = function () {
+			messageHub.postMessage("clearDetails");
+		};
+
+	}]);
---
+++ codbex-opportunities/gen/codbex-opportunities/ui/Opportunity/OpportunityNote/controller.js
@@ -0,0 +1,219 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+	.config(["messageHubProvider", function (messageHubProvider) {
+		messageHubProvider.eventIdPrefix = 'codbex-opportunities.Opportunity.OpportunityNote';
+	}])
+	.config(["entityApiProvider", function (entityApiProvider) {
+		entityApiProvider.baseUrl = "/services/ts/codbex-opportunities/gen/codbex-opportunities/api/Opportunity/OpportunityNoteService.ts";
+	}])
+	.controller('PageController', ['$scope', '$http', 'messageHub', 'entityApi', 'Extensions', function ($scope, $http, messageHub, entityApi, Extensions) {
+		//-----------------Custom Actions-------------------//
+		Extensions.get('dialogWindow', 'codbex-opportunities-custom-action').then(function (response) {
+			$scope.pageActions = response.filter(e => e.perspective === "Opportunity" && e.view === "OpportunityNote" && (e.type === "page" || e.type === undefined));
+			$scope.entityActions = response.filter(e => e.perspective === "Opportunity" && e.view === "OpportunityNote" && e.type === "entity");
+		});
+
+		$scope.triggerPageAction = function (action) {
+			messageHub.showDialogWindow(
+				action.id,
+				{},
+				null,
+				true,
+				action
+			);
+		};
+
+		$scope.triggerEntityAction = function (action) {
+			messageHub.showDialogWindow(
+				action.id,
+				{
+					id: $scope.entity.Id
+				},
+				null,
+				true,
+				action
+			);
+		};
+		//-----------------Custom Actions-------------------//
+
+		function resetPagination() {
+			$scope.dataPage = 1;
+			$scope.dataCount = 0;
+			$scope.dataLimit = 10;
+		}
+		resetPagination();
+
+		//-----------------Events-------------------//
+		messageHub.onDidReceiveMessage("codbex-opportunities.Opportunity.${masterEntity}.entitySelected", function (msg) {
+			resetPagination();
+			$scope.selectedMainEntityId = msg.data.selectedMainEntityId;
+			$scope.loadPage($scope.dataPage);
+		}, true);
+
+		messageHub.onDidReceiveMessage("codbex-opportunities.Opportunity.${masterEntity}.clearDetails", function (msg) {
+			$scope.$apply(function () {
+				resetPagination();
+				$scope.selectedMainEntityId = null;
+				$scope.data = null;
+			});
+		}, true);
+
+		messageHub.onDidReceiveMessage("clearDetails", function (msg) {
+			$scope.$apply(function () {
+				$scope.entity = {};
+				$scope.action = 'select';
+			});
+		});
+
+		messageHub.onDidReceiveMessage("entityCreated", function (msg) {
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+
+		messageHub.onDidReceiveMessage("entityUpdated", function (msg) {
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+
+		messageHub.onDidReceiveMessage("entitySearch", function (msg) {
+			resetPagination();
+			$scope.filter = msg.data.filter;
+			$scope.filterEntity = msg.data.entity;
+			$scope.loadPage($scope.dataPage, $scope.filter);
+		});
+		//-----------------Events-------------------//
+
+		$scope.loadPage = function (pageNumber, filter) {
+			let ${masterEntityId} = $scope.selectedMainEntityId;
+			$scope.dataPage = pageNumber;
+			if (!filter && $scope.filter) {
+				filter = $scope.filter;
+			}
+			if (!filter) {
+				filter = {};
+			}
+			if (!filter.$filter) {
+				filter.$filter = {};
+			}
+			if (!filter.$filter.equals) {
+				filter.$filter.equals = {};
+			}
+			filter.$filter.equals.${masterEntityId} = ${masterEntityId};
+			entityApi.count(filter).then(function (response) {
+				if (response.status != 200) {
+					messageHub.showAlertError("OpportunityNote", `Unable to count OpportunityNote: '${response.message}'`);
+					return;
+				}
+				if (response.data) {
+					$scope.dataCount = response.data;
+				}
+				filter.$offset = (pageNumber - 1) * $scope.dataLimit;
+				filter.$limit = $scope.dataLimit;
+				entityApi.search(filter).then(function (response) {
+					if (response.status != 200) {
+						messageHub.showAlertError("OpportunityNote", `Unable to list/filter OpportunityNote: '${response.message}'`);
+						return;
+					}
+
+					response.data.forEach(e => {
+						if (e.Timestamp) {
+							e.Timestamp = new Date(e.Timestamp);
+						}
+					});
+
+					$scope.data = response.data;
+				});
+			});
+		};
+
+		$scope.selectEntity = function (entity) {
+			$scope.selectedEntity = entity;
+		};
+
+		$scope.openDetails = function (entity) {
+			$scope.selectedEntity = entity;
+			messageHub.showDialogWindow("OpportunityNote-details", {
+				action: "select",
+				entity: entity,
+				optionsOpportunity: $scope.optionsOpportunity,
+			});
+		};
+
+		$scope.openFilter = function (entity) {
+			messageHub.showDialogWindow("OpportunityNote-filter", {
+				entity: $scope.filterEntity,
+				optionsOpportunity: $scope.optionsOpportunity,
+			});
+		};
+
+		$scope.createEntity = function () {
+			$scope.selectedEntity = null;
+			messageHub.showDialogWindow("OpportunityNote-details", {
+				action: "create",
+				entity: {},
+				selectedMainEntityKey: "${masterEntityId}",
+				selectedMainEntityId: $scope.selectedMainEntityId,
+				optionsOpportunity: $scope.optionsOpportunity,
+			}, null, false);
+		};
+
+		$scope.updateEntity = function (entity) {
+			messageHub.showDialogWindow("OpportunityNote-details", {
+				action: "update",
+				entity: entity,
+				selectedMainEntityKey: "${masterEntityId}",
+				selectedMainEntityId: $scope.selectedMainEntityId,
+				optionsOpportunity: $scope.optionsOpportunity,
+			}, null, false);
+		};
+
+		$scope.deleteEntity = function (entity) {
+			let id = entity.Id;
+			messageHub.showDialogAsync(
+				'Delete OpportunityNote?',
+				`Are you sure you want to delete OpportunityNote? This action cannot be undone.`,
+				[{
+					id: "delete-btn-yes",
+					type: "emphasized",
+					label: "Yes",
+				},
+				{
+					id: "delete-btn-no",
+					type: "normal",
+					label: "No",
+				}],
+			).then(function (msg) {
+				if (msg.data === "delete-btn-yes") {
+					entityApi.delete(id).then(function (response) {
+						if (response.status != 204) {
+							messageHub.showAlertError("OpportunityNote", `Unable to delete OpportunityNote: '${response.message}'`);
+							return;
+						}
+						$scope.loadPage($scope.dataPage, $scope.filter);
+						messageHub.postMessage("clearDetails");
+					});
+				}
+			});
+		};
+
+		//----------------Dropdowns-----------------//
+		$scope.optionsOpportunity = [];
+
+
+		$http.get("/services/ts/codbex-opportunities/gen/codbex-opportunities/api/Opportunity/OpportunityService.ts").then(function (response) {
+			$scope.optionsOpportunity = response.data.map(e => {
+				return {
+					value: e.Id,
+					text: e.Name
+				}
+			});
+		});
+
+		$scope.optionsOpportunityValue = function (optionKey) {
+			for (let i = 0; i < $scope.optionsOpportunity.length; i++) {
+				if ($scope.optionsOpportunity[i].value === optionKey) {
+					return $scope.optionsOpportunity[i].text;
+				}
+			}
+			return null;
+		};
+		//----------------Dropdowns-----------------//
+
+	}]);
